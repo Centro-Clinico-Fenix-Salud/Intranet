15243052384d02437d7240ded881da7d3746b0e4
aitorb43 <aitorb43@gmail.com>
2024-03-01 13:49:25 -0400

    calendario v1

---
+++ Intranet/Intranet/wwwroot/Archivo.js
@@ -0,0 +1,93 @@
+function showAlert() {
+    alert("¡Hola desde JavaScript!");
+}
+
+window.CambiarColorDiv = function (divId, color) {
+    var div = document.getElementById(divId);
+    if (div) {
+        div.style.backgroundColor = color;
+    }
+};
+
+function showAlert2() {
+
+    var calendarEl = document.getElementById('calendar');
+
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+        },
+        initialDate: '2024-03-01',
+        navLinks: true, // can click day/week names to navigate views
+        businessHours: true, // display business hours
+        editable: true,
+        locale: 'es',
+        buttonText: {
+            today: 'Hoy',
+            month: 'Mes',
+            week: 'Semana',
+            day: 'Dia'
+        },
+        selectable: true,
+        events: [
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-04'
+            },
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-04'
+            },
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-01'
+            },
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-15'
+            },
+
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-15'
+            },
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-15'
+            },
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-30'
+            },
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-09'
+            }
+            ,
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-26'
+            }
+            ,
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-11'
+            }
+            ,
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-17'
+            }
+            ,
+            {
+                title: 'Aitor Blanco - Tecnologia',
+                start: '2024-03-22'
+            }
+        ]
+    });
+
+    calendar.render();
+    
+}

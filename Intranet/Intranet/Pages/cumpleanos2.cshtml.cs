21b35f61b581cef9c55352febf96dc56e6d50fbf
aitorb43 <aitorb43@gmail.com>
2024-03-01 09:20:14 -0400

    creando cshtml

---
+++ Intranet/Intranet/Pages/cumpleanos2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Intranet.Pages
+{
+    public class calendario2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

95cdb56641da94ae3023a2e90cbb378935c84205
Jesús Carreño <j.carreno.itech@gmail.com>
2024-04-15 08:16:59 -0400

    update 15/04/24

--- Intranet/Intranet/obj/Debug/net7.0/Intranet.RazorTargetAssemblyInfo.cs
+++ Intranet/Intranet/obj/Debug/net7.0/Intranet.RazorTargetAssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intranet")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+150da507f806139920cd027c00d144e436b3ebd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afc20656f76049d87a00cb4ab822b4a75615b675")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intranet.Views")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 

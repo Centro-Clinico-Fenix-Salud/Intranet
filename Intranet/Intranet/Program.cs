c996299e0ac086e750f5f62a7544f5571c9b8668
aitorb43 <aitorb43@gmail.com>
2024-04-22 16:09:04 -0400

    creacion de tabla usuario, rol y data inicial

--- Intranet/Intranet/Program.cs
+++ Intranet/Intranet/Program.cs
@@ -15,6 +15,8 @@ using MudBlazor.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.RegisterDbContext();
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
@@ -26,6 +28,7 @@ builder.Services.AddScoped<AuthenticationStateProvider, AutenticacionExtension>(
 builder.Services.AddAuthenticationCore();
 
 var app = builder.Build();
+app.ExecuteMigrations();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -45,6 +48,9 @@ app.UseRouting();
 app.UseAuthentication();
 
 app.UseAuthorization();
+
+// insercion de super admin si no hay usuarios registrados
+new CreacionSuperAdmin(builder.Configuration["usuarioAdmin"]);
 
 app.MapBlazorHub();
 app.MapControllers();

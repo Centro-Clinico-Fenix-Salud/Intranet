3f1df56463dcf0e8f21948107f1437fb4fb1be4d
aitorb43 <aitorb43@gmail.com>
2024-04-30 13:11:04 -0400

    publicacion 29/04/2024

--- Intranet/Intranet/obj/Release/net7.0/PubTmp/Out/wwwroot/Archivo.js
+++ Intranet/Intranet/obj/Release/net7.0/PubTmp/Out/wwwroot/Archivo.js
@@ -241,4 +241,125 @@ function convertirFechaAHora(fechaFullCalendar) {
     return fechaFormateada;
 }
 
+let signaturePad = null;
+
+window.ActivarCanvas = async () => {
+
+        const canvas = document.querySelector("canvas");
+        canvas.height = canvas.offsetHeight;
+        canvas.width = canvas.offsetWidth;
+
+    signaturePad = new SignaturePad(canvas, {});
+
+    //const image = await loadImage(savePath);
+    //console.log(image);
+}
+
+function loadImage(url) {
+    return new Promise(resolve => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = "blob";
+        xhr.onload = function (e) {
+            const reader = new FileReader();
+            reader.onload = function (event) {
+                const res = event.target.result;
+                resolve(res);
+            }
+            const file = this.response;
+            reader.readAsDataURL(file);
+        }
+        xhr.send();
+    });
+}
+
+window.generatePDF = async (savePath) => {
+
+    // Bueno
+    const image = await loadImage(savePath);
+    const signatureImage = signaturePad.toDataURL();
+
+    const doc = new jsPDF('p', 'pt', 'letter');
+    doc.addImage(image, 'PNG', 0, 0, 565, 792);
+    doc.addImage(signatureImage, 'PNG', 200, 605, 300, 60);
+
+    //obteniendo data 
+
+    let curso = document.getElementById('curso').value;
+    let nombres = document.getElementById('nombre').value;
+    let apellidos = document.getElementById('apellido').value;
+    let email = document.getElementById('email').value;
+    let direccion = document.getElementById('direccion').value;
+    let telefono = document.getElementById('telefono').value;
+    let hijos = document.querySelector('input[name="hijos"]:checked').value;
+    let numeroHijos = document.getElementById('numeroHijos').value;
+    let discapacidad = document.querySelector('input[name="discapacidad"]:checked').value;
+    let discapacidadDesc = document.getElementById('discapacidad-desc').value;
+
+
+    doc.setFontSize(12);
+    doc.text(curso, 260, 125);
+
+    const date = new Date();
+    doc.text(date.getUTCDate().toString(), 235, 150);
+    doc.text((date.getUTCMonth() + 1).toString(), 275, 150);
+    doc.text(date.getUTCFullYear().toString(), 320, 150);
+
+    doc.setFontSize(10);
+    doc.text(nombres, 170, 213);
+    doc.text(apellidos, 170, 200);
+    doc.text(direccion, 170, 400);
+    doc.text(telefono, 170, 456);
+    doc.text(email, 170, 475);
+
+    doc.setFillColor(0, 0, 0);
+
+    if (parseInt(hijos) === 0) {
+        doc.circle(255, 374, 4, 'FD');
+    } else {
+        doc.circle(190, 374, 4, 'FD');
+        doc.text(numeroHijos.toString(), 355, 378);
+    }
+
+    if (parseInt(discapacidad) === 0) {
+        doc.circle(285, 718, 4, 'FD');
+    } else {
+        doc.circle(240, 718, 4, 'FD');
+        doc.text(discapacidadDesc, 350, 720);
+    }
+
+    //doc.save("example.pdf"); 
+
+    //return
+    //var pdf = new jsPDF();
+    //pdf.text(30, 30, 'hello world');
+    //pdf.save("example.pdf");
+
+    // Generar PDF con jsPDF
+    //var doc = new jsPDF();
+
+    // Agregar contenido al documento
+   // doc.text('Hola, este es un PDF generado con jsPDF', 10, 10);
+
+    // Guardar el documento en una variable
+    var pdfData = doc.output('blob');
+
+    // Crear un objeto URL para el blob del PDF
+    var pdfUrl = URL.createObjectURL(pdfData);
+
+    // Obtener el elemento del DOM con el id "PrevisualizacionPDF"
+    var previsualizacionPDF = document.getElementById('PrevisualizacionPDF');
+
+    // Limpiar el contenido existente del div
+    previsualizacionPDF.innerHTML = ''; // Vaciar el contenido HTML
+
+    // Mostrar el PDF en un elemento iframe
+    var iframe = document.createElement('iframe');
+    iframe.src = pdfUrl;
+    iframe.style.width = '100%';
+    iframe.style.height = '700px'; // Establecer la altura deseada
+    previsualizacionPDF.appendChild(iframe);
+
+}
+
 

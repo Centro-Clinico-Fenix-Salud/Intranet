f304071b83154cba8c6473b20e0e8e5f68e67e66
Jesús Carreño <j.carreno.itech@gmail.com>
2024-05-29 14:22:11 -0400

    1

--- Intranet/Intranet/obj/Debug/net7.0/RazorDeclaration/Pages/InspeccionHabitacion.razor.g.cs
+++ Intranet/Intranet/obj/Debug/net7.0/RazorDeclaration/Pages/InspeccionHabitacion.razor.g.cs
@@ -13,91 +13,91 @@ namespace Intranet.Pages
     using global::System.Threading.Tasks;
     using global::Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 1 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 4 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 5 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 6 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 7 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 8 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 9 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using Intranet;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 10 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using Intranet.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\_Imports.razor"
+#line 11 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\_Imports.razor"
 using MudBlazor;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
+#line 3 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
+#line 4 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
+#line 5 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
 using BlazorBootstrap;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
+#line 7 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
            [Authorize]
 
 #line default
@@ -112,7 +112,7 @@ using BlazorBootstrap;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 644 "C:\Users\programador\Desktop\intranet\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
+#line 644 "C:\Users\programador\Desktop\proyectos\Intranet\Intranet\Intranet\Pages\InspeccionHabitacion.razor"
        
     private string vacio = string.Empty;
     private string eventLog { get; set; } = $"Last event: ..., CurrentPage: 0, TotalPages: 0";
